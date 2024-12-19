d894b3f107bc4531d616af54a550c4e14bb931db
Erwin-Gerdes19 <erwin.gerdes19@gmail.com>
2024-12-19 17:20:45 -0300

    add maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+import path from 'path'
+
 export default {
     mode: 'development',
     entry: {
-
+        mapa: './src/js/mapa.js'
     },
     output: {
-
+        filenmae: '[name].js',
+        path: path.resolve('public/js') // La propiedad path sirve para no dar la ruta completa, y solamente nos manejemos con el nombre del archivo
     }
 }
