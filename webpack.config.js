c23d1ffa65d8e6bb8512da6071fd6148f04483de
Erwin-Gerdes19 <erwin.gerdes19@gmail.com>
2025-01-18 02:17:33 -0300

    cambios dia 18 de junio

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ import path from 'path'
 export default {
     mode: 'development',
     entry: {
-        mapa: './src/js/mapa.js'
+        mapa: './src/js/mapa.js',
     },
     output: {
-        filenmae: '[name].js',
+        filename: '[name].js',
         path: path.resolve('public/js') // La propiedad path sirve para no dar la ruta completa, y solamente nos manejemos con el nombre del archivo
     }
 }
